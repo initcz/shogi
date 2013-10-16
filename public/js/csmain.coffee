d4f16cb811d44681caef406892bd64e222770444
Jakub Vitak <mainiak@gmail.com>
2013-10-16 22:21:07 +0200

    preparing switch to AMD

---
+++ public/js/csmain.coffee
@@ -0,0 +1,10 @@
+define [
+  #'cs!controller'
+  #'cs!view'
+  #'regular'
+], (controller, view, regular) ->
+  ###
+  controller.attach view
+  console.log 'regular name is: ' + regular.name
+  ###
+  console.log 'hello AMD world!'
---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+/*
+ * see https://github.com/requirejs/require-cs
+ *
+ * [D3.js]
+ * http://stackoverflow.com/questions/13157704/how-to-integrate-d3-with-require-js
+ * https://groups.google.com/forum/#!topic/d3-js/PSPKF8V0D00
+ *
+ * [Zepto.js] - necessary when we want to use D3? FIXME - Ajax?
+ * http://stackoverflow.com/questions/13425815/how-to-use-requirejs-with-zepto
+ * http://simonsmith.io/using-zepto-and-jquery-with-requirejs/
+ *
+ */
+require({
+  paths: {
+    cs: './cs',
+    'coffee-script': './coffee-script'
+  }
+}, ['cs!csmain']);
